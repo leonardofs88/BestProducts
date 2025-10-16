2a6089ff6fa00d8430123c7597e805c60821668e
Leonardo Soares <leonardofs88@live.com>
2025-10-16 19:18:00 +0100

    [WIP] Image resizing accordingly with the scrolling

--- BestProducts/Views/DetailsView.swift
+++ BestProducts/Views/DetailsView.swift
@@ -8,63 +8,174 @@
 import SwiftUI
 
 struct DetailsView: View {
+    @Namespace private var namespace
+    @State private var scrollOffset: CGPoint = .zero
     private(set) var product: Product
 
     var body: some View {
-        List {
+        VStack {
             VStack(alignment: .center) {
                 if product.imagesURLs.isEmpty {
                     Image(systemName: "photo")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 200)
                 } else {
-                    ForEach(product.imagesURLs, id: \.self) { imageURL in
-                        AsyncImage(url: imageURL) { phase in
-                            if let image = phase.image {
-                                image // Displays the loaded image.
-                            } else if phase.error != nil {
-                                Color.red // Indicates an error.
-                            } else {
-                                Color.blue // Acts as a placeholder.
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(product.imagesURLs, id: \.self) { imageURL in
+                                AsyncImage(url: imageURL) { phase in
+                                    if let image = phase.image {
+                                        image // Displays the loaded image.
+                                            .resizable()
+                                            .scaledToFit()
+                                    } else if phase.error != nil {
+                                        Color.red // Indicates an error.
+                                    } else {
+                                        Color.blue // Acts as a placeholder.
+                                    }
+                                }
                             }
                         }
                     }
+                    .scrollTargetBehavior(.paging)
                 }
             }
-            .frame(maxWidth: .infinity, alignment: .center)
-            HStack {
-                Text("Name:")
-                    .fontWeight(.bold)
-                Spacer()
-                Text(product.title)
-            }
-            HStack {
-                Text("Price:")
-                    .fontWeight(.bold)
-                Spacer()
-                Text(product.price, format: .currency(code: "EUR"))
-            }
-            HStack {
-                Text("Discount:")
-                    .fontWeight(.bold)
-                Spacer()
-                Text(product.discount, format: .percent)
-            }
-            HStack {
-                Text("Stock:")
-                    .fontWeight(.bold)
-                Spacer()
-                Text(product.stock, format: .number)
-            }
-            HStack {
-                Text("Rating:")
-                    .fontWeight(.bold)
-                Spacer()
-                RatingView(rating: product.rating)
-            }
-        }
 
+            ScrollView {
+                Color.clear
+                    .scrollOffset(namespace: namespace)
+
+                HStack {
+                    Text("Name:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.title)
+                }
+                HStack {
+                    Text("Price:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.price, format: .currency(code: "EUR"))
+                }
+                HStack {
+                    Text("Discount:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.discount, format: .percent)
+                }
+                HStack {
+                    Text("Stock:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.stock, format: .number)
+                }
+                HStack {
+                    Text("Rating:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    RatingView(rating: product.rating)
+                }
+                HStack {
+                    Text("Name:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.title)
+                }
+                HStack {
+                    Text("Price:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.price, format: .currency(code: "EUR"))
+                }
+                HStack {
+                    Text("Discount:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.discount, format: .percent)
+                }
+                HStack {
+                    Text("Stock:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.stock, format: .number)
+                }
+                HStack {
+                    Text("Rating:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    RatingView(rating: product.rating)
+                }
+                HStack {
+                    Text("Name:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.title)
+                }
+                HStack {
+                    Text("Price:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.price, format: .currency(code: "EUR"))
+                }
+                HStack {
+                    Text("Discount:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.discount, format: .percent)
+                }
+                HStack {
+                    Text("Stock:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.stock, format: .number)
+                }
+                HStack {
+                    Text("Rating:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    RatingView(rating: product.rating)
+                }
+                HStack {
+                    Text("Name:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.title)
+                }
+                HStack {
+                    Text("Price:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.price, format: .currency(code: "EUR"))
+                }
+                HStack {
+                    Text("Discount:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.discount, format: .percent)
+                }
+                HStack {
+                    Text("Stock:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text(product.stock, format: .number)
+                }
+                HStack {
+                    Text("Rating:")
+                        .fontWeight(.bold)
+                    Spacer()
+                    RatingView(rating: product.rating)
+                }
+            } //: ScrollView
+            .onPreferenceChange(ViewOffsetKey.self) { value in
+                if value.y < scrollOffset.y {
+                    print("Scrolling Up")
+                } else if value.y > scrollOffset.y {
+                    print("Scrolling Down")
+                }
+                scrollOffset = value
+            }
+            .coordinateSpace(name: namespace)
+        }
     }
 }
 
