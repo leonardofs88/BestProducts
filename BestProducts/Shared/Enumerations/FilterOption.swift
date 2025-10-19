a1d3b9f2fd8b37236608402fa9c7d57e2c333aab
Leonardo Soares <leonardofs88@live.com>
2025-10-19 14:27:57 +0100

    adding missing changes

---
+++ BestProducts/Shared/Enumerations/FilterOption.swift
@@ -0,0 +1,13 @@
+//
+//  FilterOption.swift
+//  BestProducts
+//
+//  Created by Leonardo Soares on 19/10/2025.
+//
+
+enum FilterOption {
+    case caseInsensitive
+    case diacriticInsensitive
+    case all
+    case none
+}
